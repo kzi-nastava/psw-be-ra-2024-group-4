2e7065d136ec7c9c90ee76f1e976e1cbb0592291
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-10-12 18:58:11 +0200

    feat: Created KeyPoint service

---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/KeypointAddition/KeyPointService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Author;
+using Explorer.Tours.Core.Domain;
+
+namespace Explorer.Tours.Core.UseCases.Author;
+
+public class KeyPointService : CrudService<KeyPointDto, KeyPoint>, IKeyPointService
+{
+    public KeyPointService(ICrudRepository<KeyPoint> repository, IMapper mapper) : base(repository, mapper) { }
+
+}
