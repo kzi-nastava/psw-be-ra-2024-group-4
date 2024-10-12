7336c2a6294ef4a95a89079fe34392a267f62cd6
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-10-12 14:21:07 +0200

    Created KeyPoint entity

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/KeyPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+using Explorer.BuildingBlocks.Core.Domain;
+namespace Explorer.Tours.Core.Domain
+{
+    public class KeyPoint : Entity
+    {
+        public string Name { get; private set; }
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+        public string Description { get; private set; }
+        public string Image {  get; private set; }
+
+        public KeyPoint(string name, double longitude, double latitude, string description, string image)
+        {
+            ValidateArguments(name, longitude, latitude, description, image);
+            Name = name;
+            Longitude = longitude;
+            Latitude = latitude;
+            Description = description;
+            Image = image;
+
+            
+        }
+
+        public void ValidateArguments(string name, double longitude, double latitude, string description, string image)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+            if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Invalid Description.");
+            if (string.IsNullOrWhiteSpace(image)) throw new ArgumentException("Invalid Image.");
+            if (longitude <= 0) throw new ArgumentException("Invalid longitude.");
+            if (latitude <= 0) throw new ArgumentException("Invalid latitude.");
+        }
+
+
+    }
+}
