9a1363e7125302fc251433fdff5b9fef44807247
RizovicTanja <tanjarizovic0@gmail.com>
2024-10-12 16:37:37 +0200

    Implement service and repository layers for person profile, created IPersonService, PersonUpdateService, integrated DTOs

---
+++ src/Explorer.API/Controllers/Person/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Tours.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Person
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/person")]
+    public class PersonController : BaseApiController
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<PersonUpdateDto> Update([FromBody] PersonUpdateDto person)
+        {
+            var result = _personService.Update(person);
+            return CreateResponse(result);
+        }
+    }
+}
