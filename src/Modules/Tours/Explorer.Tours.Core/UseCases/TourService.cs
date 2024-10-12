dd690b590123f10f237ac94287013790fa4530ac
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2024-10-13 00:28:35 +0200

    feat: added class, dto and service

---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/TourService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases
+{
+    public class TourService : CrudService<TourDto, Tour>, ITourService
+    {
+        //ova klasa ce biti podlozna promenama.
+        //Za sada sam ovako uradila, ali kad potvrdim sa asistenom i kolegama mozda promenim.
+        //Posto koleginica zavisi od toga kad cu ja zavrsiti ja cu predati sada.
+        public TourService(ICrudRepository<Tour> repository, IMapper mapper) : base(repository, mapper) { }
+
+        public Result<List<TourDto>> GetByUserId(long userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
