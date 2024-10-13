037b20075c8ba77ec40096da39173d758a278686
Ana Edelinski <aedelinski85@gmail.com>
2024-10-13 15:08:55 +0200

    feat: TourRepository Methods AddEquipmentToTour and RemoveEquipmentFromTour

--- src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourRepository.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourRepository.cs
@@ -9,5 +9,7 @@ namespace Explorer.Tours.Core.Domain.RepositoryInterfaces
     public interface ITourRepository
     {
         List<Tour> GetToursByUserId(long userId);
+        void AddEquipmentToTour(long tourId, long equipmentId);
+        void RemoveEquipmentFromTour(long tourId, long equipmentId);
     }
 }
--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/Repositories/TourRepository.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/Repositories/TourRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Explorer.Tours.Core.Domain;
 using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,36 @@ namespace Explorer.Tours.Infrastructure.Database.Repositories
             return _dbContext.Tour
                          .Where(t => t.UserId == userId)
                          .ToList();
+        }
+
+        public void AddEquipmentToTour(long tourId, long equipmentId)
+        {
+            var tour = _dbContext.Tour
+                                .Single(t => t.Id == tourId);
+            var equipment = _dbContext.Equipment
+                                     .Single(e => e.Id == equipmentId);
+
+            if (tour.EquipmentIds.Contains(equipment.Id))
+            {
+                throw new InvalidOperationException("This equipment is already added to the tour.");
+            }
+            tour.EquipmentIds.Add(equipment.Id);
+
+            _dbContext.SaveChanges();
+        }
+        public void RemoveEquipmentFromTour(long tourId, long equipmentId)
+        {
+            var tour = _dbContext.Tour
+                                .Single(t => t.Id == tourId);
+            var equipment = _dbContext.Equipment
+                                     .Single(e => e.Id == equipmentId);
+
+            if (!tour.EquipmentIds.Contains(equipmentId))
+            {
+                throw new InvalidOperationException("This equipment is not associated with the tour.");
+            }
+            tour.EquipmentIds.Remove(equipment.Id);
+            _dbContext.SaveChanges();
         }
     }
 }
