6cea2dc75f93f581f3f26c6b8642c30e24086cdc
Damjan Ilic <damjanilic02@gmail.com>
2024-10-14 22:17:43 +0200

    feat: adds domain class, services, interfaces and Dtos

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/ClubJoinRequestService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.UseCases
+{
+    public class ClubJoinRequestService : CrudService<ClubJoinRequestDto, ClubJoinRequest>, IClubJoinRequestService
+    {
+        public ClubJoinRequestService(ICrudRepository<ClubJoinRequest> repository, IMapper mapper): base(repository, mapper) { }
+    }
+}
