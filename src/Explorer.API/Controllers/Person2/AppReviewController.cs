432fe5d1f0e41b5ab3da2ea0944e1aabf875830c
Maša Mastilović <masamastilovic@gmail.com>
2024-10-14 11:04:03 +0200

    AppReview Controllers

---
+++ src/Explorer.API/Controllers/Person2/AppReviewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.API.Dtos;
+
+namespace Explorer.API.Controllers.Person2
+{
+    [Route("api/person/appReview")]
+    public class AppReviewController: BaseApiController
+    {
+        private readonly IAppReviewService _appReviewService;
+
+        public AppReviewController(IAppReviewService appReviewService)
+        {
+            _appReviewService = appReviewService;
+        }
+
+
+        [HttpPost]
+        public ActionResult<AppReviewDto> Create([FromBody] AppReviewDto appReview)
+        {
+            var result = _appReviewService.Create(appReview);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<AppReviewDto> Update([FromBody] AppReviewDto appReview)
+        {
+            var result = _appReviewService.Update(appReview);
+            return CreateResponse(result);
+        }
+    }
+}
