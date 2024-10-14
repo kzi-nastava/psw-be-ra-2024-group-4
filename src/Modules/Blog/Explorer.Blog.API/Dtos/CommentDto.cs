7c035f2e2e4f05134499685878e2ebc3985d3759
Arijana Marić <maricarijana69@gmail.com>
2024-10-14 19:22:13 +0200

    feat: added dtos and services

---
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/CommentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.API.Dtos
+{
+    public class CommentDto
+    {
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public long UserId { get; set; }
+        public long BlogId { get; set; }
+    }
+}
---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Comment.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Explorer.Blog.Core.Domain;
+
+    public class Comment: Entity
+    {
+        public string Text { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set;}
+        public long UserId { get; private set; }
+        public long BlogId { get; private set; }
+
+    public Comment(string text, DateTime createdAt, DateTime? updatedAt, long userId, long blogId)
+    {
+        Text = text;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+        UserId = userId;
+        BlogId = blogId;
+        Validate();
+    }
+    private void Validate() {
+
+        if (string.IsNullOrWhiteSpace(Text)) throw new ArgumentException("Invalid Text.");
+        if (CreatedAt == default(DateTime)) throw new ArgumentException("Invalid CreatedAt date");
+        if (UpdatedAt == default(DateTime)) throw new ArgumentException("Invalid UpdatedAt date");
+        if (UserId == 0) throw new ArgumentException("Invalid UserId.");
+        if (BlogId == 0) throw new ArgumentException("Invalid BlogId.");
+    }
+}
+
