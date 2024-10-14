f3f427180443ad9ae0458090aa2793c8c5911439
RizovicTanja <tanjarizovic0@gmail.com>
2024-10-14 19:58:56 +0200

    Test: Add unit and integration tests for user profile features

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/PersonTests/PersonTests.cs
@@ -0,0 +1,103 @@
+﻿
+using Explorer.API.Controllers.Person;
+using Explorer.Stakeholders.API.Dtos; 
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Infrastructure.Database;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Stakeholders.Tests.Integration.PersonTests;
+
+[Collection("Sequential")]
+public class PersonTests : BaseStakeholdersIntegrationTest
+{
+    public PersonTests(StakeholdersTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Updates()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+        var updatedEntity = new PersonUpdateDto
+        {
+            Id = 5,
+            UserId = 5,
+            Name = "string",
+            Surname = "string",
+            Email = "string@gmail.com",
+            ProfilePicture = "string",
+            Biography = "string",
+            Motto = "string"
+        };
+
+        // Act
+        var result = ((ObjectResult)controller.Update(updatedEntity).Result)?.Value as PersonUpdateDto;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(updatedEntity.Id);
+        result.UserId.ShouldBe(updatedEntity.UserId);
+        result.Name.ShouldBe(updatedEntity.Name);
+        result.Surname.ShouldBe(updatedEntity.Surname);
+        result.Email.ShouldBe(updatedEntity.Email);
+        result.ProfilePicture.ShouldBe(updatedEntity.ProfilePicture);
+        result.Biography.ShouldBe(updatedEntity.Biography);
+        result.Motto.ShouldBe(updatedEntity.Motto);
+
+        // Assert - Database
+        var storedEntity = dbContext.People.FirstOrDefault(i => i.Email == updatedEntity.Email);
+        storedEntity.Id.ShouldBe(updatedEntity.Id);
+        storedEntity.ShouldNotBeNull();
+        storedEntity.Name.ShouldBe(updatedEntity.Name);
+        storedEntity.Surname.ShouldBe(updatedEntity.Surname);
+        storedEntity.Biography.ShouldBe(updatedEntity.Biography);
+
+        var oldEntity = dbContext.People.FirstOrDefault(i => i.Email == "string@gmail.com");
+        oldEntity.ShouldBeNull();
+    }
+
+    //[Fact]
+    //public void GetById_Returns_CorrectPersonFromDatabase()
+    //{
+    //    // Arrange
+    //    using var scope = Factory.Services.CreateScope();
+    //    var controller = CreateController(scope);
+    //    var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+
+    //    var existingPersonId = 5;
+
+    //    // Act - pozivamo Get metodu
+    //    var result = controller.Get(existingPersonId); // Očekujemo Result<PersonUpdateDto>
+
+    //    // Assert
+    //    result.ShouldNotBeNull();
+    //    result.IsSuccess.ShouldBeTrue(); // Proverava da li je operacija bila uspešna
+
+    //    var personDto = result.Value; // Dobijanje DTO objekta iz rezultata
+    //    personDto.ShouldNotBeNull();
+    //    personDto.Id.ShouldBe(existingPersonId);
+
+    //    var storedEntity = dbContext.People.FirstOrDefault(i => i.Id == existingPersonId);
+    //    storedEntity.ShouldNotBeNull();
+    //    personDto.Name.ShouldBe(storedEntity.Name);
+    //    personDto.Surname.ShouldBe(storedEntity.Surname);
+    //    personDto.Email.ShouldBe(storedEntity.Email);
+    //    personDto.ProfilePicture.ShouldBe(storedEntity.ProfilePicture);
+    //    personDto.Biography.ShouldBe(storedEntity.Biography);
+    //    personDto.Motto.ShouldBe(storedEntity.Motto);
+    //}
+
+
+    private static PersonController CreateController(IServiceScope scope)
+    {
+        return new PersonController(scope.ServiceProvider.GetRequiredService<IPersonService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/c-people.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/c-people.sql
@@ -1,19 +1,24 @@
 ﻿INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-11, -11, 'Ana', 'Anić', 'autor1@gmail.com');
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-11, -11, 'Ana', 'Anić', 'autor1@gmail.com', 'profilepic1.jpg', 'Ana is a talented author.', 'Live and learn.');
 INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-12, -12, 'Lena', 'Lenić', 'autor2@gmail.com');
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-12, -12, 'Lena', 'Lenić', 'autor2@gmail.com', 'profilepic2.jpg', 'Lena loves to write about travel.', 'Explore the world.');
 INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-13, -13, 'Sara', 'Sarić', 'autor3@gmail.com');
-
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-13, -13, 'Sara', 'Sarić', 'autor3@gmail.com', 'profilepic3.jpg', 'Sara is a creative writer.', 'Imagination is key.');
 INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-21, -21, 'Pera', 'Perić', 'turista1@gmail.com');
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-21, -21, 'Pera', 'Perić', 'turista1@gmail.com', 'profilepic4.jpg', 'Pera enjoys discovering new places.', 'Adventure awaits.');
 INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-22, -22, 'Mika', 'Mikić', 'turista2@gmail.com');
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-22, -22, 'Mika', 'Mikić', 'turista2@gmail.com', 'profilepic5.jpg', 'Mika is an avid traveler.', 'Travel more, worry less.');
 INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email")
-VALUES (-23, -23, 'Steva', 'Stević', 'turista3@gmail.com');
+    "Id", "UserId", "Name", "Surname", "Email", "ProfilePicture", "Biography", "Motto")
+VALUES 
+(-23, -23, 'Steva', 'Stević', 'turista3@gmail.com', 'profilepic6.jpg', 'Steva enjoys nature and hiking.', 'Nature is the best teacher.');
