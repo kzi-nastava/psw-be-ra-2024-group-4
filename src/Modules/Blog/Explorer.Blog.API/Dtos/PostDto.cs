1c6abfc9680929b8aa2f779dca6d2d29a9c19bad
Anja Srdić <srdicanjaa31@gmail.com>
2024-10-14 18:32:08 +0200

    feat: Model, Iservice, Service and Dto added

---
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/PostDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Explorer.Blog.API.Dtos
+{
+    public class PostDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ImageUrl { get; set; }
+        public long UserId { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        Draft,
+        Published,
+        Closed
+    }
+}
