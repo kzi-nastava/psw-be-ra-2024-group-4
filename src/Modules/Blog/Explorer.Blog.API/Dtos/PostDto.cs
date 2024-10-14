0c0bbba8f59dc4b26f66358c740e22e53c4a39bc
Anja Srdić <srdicanjaa31@gmail.com>
2024-10-14 22:58:49 +0200

    feat: PostController added,infrastructure configured

--- src/Modules/Blog/Explorer.Blog.API/Dtos/PostDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/PostDto.cs
@@ -4,6 +4,7 @@ namespace Explorer.Blog.API.Dtos
 {
     public class PostDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
