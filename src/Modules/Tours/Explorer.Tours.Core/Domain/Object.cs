6f5bfba0bd3d1dd79bad5224b0cf22a9206f0317
bteodora <149783651+bteodora@users.noreply.github.com>
2024-10-14 16:53:55 +0200

    Create Object.cs

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Object.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public enum ObjectCategory { WC, Restaurant, Parking, Other}
+
+    public class Object: Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public ObjectCategory Category { get; private set; }
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+        public long UserId { get; private set; }
+
+        public Object(string name, string description, string image, ObjectCategory category, double longitude, double latitude, long userId)
+        {
+            Validate(name, description, image, category, longitude, latitude, userId);
+            Name = name;
+            Description = description;
+            Image = image;
+            Category = category;
+            Longitude = longitude;
+            Latitude = latitude;
+            UserId = userId;
+        }
+
+        public void Validate(string name, string description, string image, ObjectCategory category, double longitude, double latitude, long userId)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Invalid Name.");
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Invalid Description.");
+
+            if (string.IsNullOrWhiteSpace(image))
+                throw new ArgumentException("Invalid Image.");
+
+            if (longitude < -180 || longitude > 180)
+                throw new ArgumentException("Longitude must be between -180 and 180 degrees.");
+
+            if (latitude < -90 || latitude > 90)
+                throw new ArgumentException("Latitude must be between -90 and 90 degrees.");
+
+            if (!Enum.IsDefined(typeof(ObjectCategory), category))
+                throw new ArgumentException("Invalid Category.");
+
+            if (userId <= 0)
+                throw new ArgumentException("Invalid user.");
+        }
+
+    }
+}
