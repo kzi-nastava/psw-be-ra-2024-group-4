617d552fb4a8cba68281e438b80d3f31ab0cc665
Arijana Marić <maricarijana69@gmail.com>
2024-10-14 20:35:40 +0200

    feat: dto and services 2

--- src/Modules/Blog/Explorer.Blog.API/Dtos/CommentDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/CommentDto.cs
@@ -8,10 +8,11 @@ namespace Explorer.Blog.API.Dtos
 {
     public class CommentDto
     {
+        public int Id { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public long UserId { get; set; }
-        public long BlogId { get; set; }
+        public long PostId { get; set; }
     }
 }
--- src/Modules/Blog/Explorer.Blog.Core/Domain/Comment.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Comment.cs
@@ -15,15 +15,15 @@ namespace Explorer.Blog.Core.Domain;
         public DateTime CreatedAt { get; private set; }
         public DateTime? UpdatedAt { get; private set;}
         public long UserId { get; private set; }
-        public long BlogId { get; private set; }
+        public long PostId { get; private set; }
 
-    public Comment(string text, DateTime createdAt, DateTime? updatedAt, long userId, long blogId)
+    public Comment(string text, DateTime createdAt, DateTime? updatedAt, long userId, long postId)
     {
         Text = text;
         CreatedAt = createdAt;
         UpdatedAt = updatedAt;
         UserId = userId;
-        BlogId = blogId;
+        PostId = postId;
         Validate();
     }
     private void Validate() {
@@ -32,7 +32,7 @@ namespace Explorer.Blog.Core.Domain;
         if (CreatedAt == default(DateTime)) throw new ArgumentException("Invalid CreatedAt date");
         if (UpdatedAt == default(DateTime)) throw new ArgumentException("Invalid UpdatedAt date");
         if (UserId == 0) throw new ArgumentException("Invalid UserId.");
-        if (BlogId == 0) throw new ArgumentException("Invalid BlogId.");
+        if (PostId == 0) throw new ArgumentException("Invalid PostId.");
     }
 }
 
