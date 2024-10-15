cc4aa7e8a47211ab722ef212ef67d54d9a1e747e
bteodora <149783651+bteodora@users.noreply.github.com>
2024-10-15 22:48:57 +0200

    Update d-objects.sql

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/d-objects.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/d-objects.sql
@@ -1,9 +1,9 @@
 INSERT INTO tours."Objects"(
     "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId")
-VALUES (1, 'Restoran kod Miloša', 'Tradicionalni srpski restoran poznat po specijalitetima sa roštilja.', 'restoran_milos.jpg', 'Restaurant', 20.457273, 44.817613, 1001);
+VALUES (-1, 'Restoran kod Miloša', 'Tradicionalni srpski restoran poznat po specijalitetima sa roštilja.', 'restoran_milos.jpg', 'Restaurant', 20.457273, 44.817613, 1001);
 INSERT INTO tours."Objects"(
     "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId")
-VALUES (2, 'Parking Slavija', 'Javni parking sa preko 200 mesta u centru Beograda.', 'parking_slavija.jpg', 'Parking', 20.467327, 44.799935, 1002);
+VALUES (-2, 'Parking Slavija', 'Javni parking sa preko 200 mesta u centru Beograda.', 'parking_slavija.jpg', 'Parking', 20.467327, 44.799935, 1002);
 INSERT INTO tours."Objects"(
     "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId")
-VALUES (3, 'WC Kalemegdan', 'Javni toalet na ulazu u park Kalemegdan.', 'wc_kalemegdan.jpg', 'WC', 20.448921, 44.823292, 1003);
+VALUES (-3, 'WC Kalemegdan', 'Javni toalet na ulazu u park Kalemegdan.', 'wc_kalemegdan.jpg', 'WC', 20.448921, 44.823292, 1003);
