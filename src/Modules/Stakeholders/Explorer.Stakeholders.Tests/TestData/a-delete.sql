041d3ec6da1aa4dd08b7d8b35d437ae75c44bab0
Irina Poposki <ninapoposki6@gmail.com>
2024-10-20 12:29:05 +0200

    changing delete sql

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
@@ -1,7 +1,4 @@
-﻿DELETE FROM stakeholders."People";
-DELETE FROM stakeholders."Users";
-<<<<<<< HEAD
-DELETE FROM stakeholders."ClubInvitations";
-=======
+﻿DELETE FROM stakeholders."ClubInvitations";
 DELETE FROM stakeholders."Clubs";
->>>>>>> 69a99637b4bb3f0770760c4f5e578bfb7b60d248
+DELETE FROM stakeholders."People";
+DELETE FROM stakeholders."Users";
