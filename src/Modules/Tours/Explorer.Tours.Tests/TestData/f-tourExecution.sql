b9930eaeb77abb60cb578af9f2f637bfff1653a7
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2024-10-31 20:38:27 +0100

    fixed tests

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/f-tourExecution.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/f-tourExecution.sql
@@ -1,18 +1,18 @@
 INSERT INTO tours."TourExecution"(
     "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
 VALUES 
-    (-1, -1, -22, 101, '2024-10-14 23:53:24.948+02', 0, 
+    (-1, -1, -22, -1, '2024-10-14 23:53:24.948+02', 0, 
         '[]');
 
 INSERT INTO tours."TourExecution"(
     "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
 VALUES 
-    (-2, -1, -21, 102, '2024-10-14 23:53:24.948+02', 1, 
+    (-2, -1, -21, -2, '2024-10-14 23:53:24.948+02', 1, 
         '[]');
 
 INSERT INTO tours."TourExecution"(
     "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
 VALUES 
-    (-3, -2, -23, 103, '2024-10-14 23:53:24.948+02', 0, 
+    (-3, -2, -23, -1, '2024-10-14 23:53:24.948+02', 0, 
         '[]');
 
