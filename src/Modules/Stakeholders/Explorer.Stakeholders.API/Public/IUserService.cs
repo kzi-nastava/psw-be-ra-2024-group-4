9ffb6a229700111f8d8f48769126fbbf260ddc1e
stasaaa <radojicic.stasa@gmail.com>
2024-11-03 21:04:41 +0100

    added getUsername in user service

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IUserService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IUserService.cs
@@ -15,5 +15,6 @@ namespace Explorer.Stakeholders.API.Public
         Result<UserDto> Update(UserDto dto);
         Result<PagedResult<UserDto>> GetPaged(int page, int pageSize);
         Result Delete(int id);
+        Result<UserDto> GetUsername(long id);
     }
 }
