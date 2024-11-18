d8a170ee89ee13cced4870001d8951d75e608c10
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-11-18 18:53:46 +0100

    Added DatabaseInitializer

--- src/Explorer.API/Program.cs
+++ src/Explorer.API/Program.cs
@@ -1,4 +1,5 @@
 using Explorer.API.Startup;
+using Explorer.API.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +10,31 @@ builder.Services.ConfigureCors(corsPolicy);
 builder.Services.ConfigureAuth();
 
 builder.Services.RegisterModules();
+builder.Services.AddTransient<DatabaseInitializer>();
 
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        var initializer = services.GetRequiredService<DatabaseInitializer>();
+        initializer.StakeholdersDatabaseAsync(services).GetAwaiter().GetResult();
+        initializer.ToursDatabaseAsync(services).GetAwaiter().GetResult();
+        initializer.BlogsDatabaseAsync(services).GetAwaiter().GetResult();
+        initializer.PaymentsDatabaseAsync(services).GetAwaiter().GetResult();
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "An error occurred while seeding the database.");
+        throw;
+    }
+}
+
 
 if (app.Environment.IsDevelopment())
 {
