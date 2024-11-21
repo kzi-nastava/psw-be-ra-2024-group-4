fdc2df0b3f219228942a41ad605d1b5f24e52d28
bteodora <149783651+bteodora@users.noreply.github.com>
2024-11-21 03:04:12 +0100

    feat: filled in the Encounter.Core layer with DTOs, Repo interfaces and services

---
+++ src/Explorer.Encounter.Core/UseCases/EncounterService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Encounter.API.Dtos.Explorer.Encounters.API.Dtos;
+using Explorer.Encounter.Core.Domain.RepositoryInterfaces;
+using Explorer.Encounter.API.Public;
+
+namespace Explorer.Encounter.Core.UseCases
+{
+    public class EncounterService : CrudService<EncounterDto, Domain.Encounter>, IEncounterService
+    {
+        private IEncounterRepository _encounterRepository;
+        public EncounterService(ICrudRepository<Domain.Encounter> crudRepository, IMapper mapper, IEncounterRepository encounterRepository) : base(crudRepository, mapper)
+        {
+            _encounterRepository = encounterRepository;
+        }
+    }
+}
