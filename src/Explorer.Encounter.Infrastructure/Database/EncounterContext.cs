b58c4ac7f365b07fddc923f54afeb275b8d641d1
bteodora <149783651+bteodora@users.noreply.github.com>
2024-11-21 03:05:06 +0100

    feat: filled in the Encounter.Infrastructure layer

---
+++ src/Explorer.Encounter.Infrastructure/Database/EncounterContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounter.Infrastructure.Database
+{
+    public class EncounterContext : DbContext
+    {
+        public EncounterContext(DbContextOptions<EncounterContext> options) : base(options) { }
+
+        
+        public DbSet<Core.Domain.Encounter> Encounters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.HasDefaultSchema("encounter");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
