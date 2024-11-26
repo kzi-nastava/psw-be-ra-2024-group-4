27bbc0d130622c18fbd5f05d254eed10d16bf7fc
Anja Srdić <srdicanjaa31@gmail.com>
2024-11-26 17:48:41 +0100

    test:: Added tests for update,delete and query tests for getAll and getByTourID

--- src/Modules/Payments/Explorer.Payments.Tests/Integration/CouponCommandTests.cs
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/CouponCommandTests.cs
@@ -34,7 +34,7 @@ namespace Explorer.Payments.Tests.Integration
             {
                 DiscountPercentage = 10,
                 ExpirationDate = new DateTime(2024, 12, 31, 12, 30, 0, DateTimeKind.Utc),
-                AuthorId = 1,
+                AuthorId = 2,
                 TourId = 1
             };
             var result = ((ObjectResult)controller.Create(newEntity).Result)?.Value as CouponDto;
@@ -50,6 +50,78 @@ namespace Explorer.Payments.Tests.Integration
             var storedEntity = dbContext.Coupons.FirstOrDefault(i => i.PromoCode == newEntity.PromoCode); //?
             storedEntity.ShouldNotBeNull();
             storedEntity.Id.ShouldBe(result.Id);
+        }
+        [Fact]
+        public void Create_fails_invalid_data()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var updatedEntity = new CouponDto
+            {
+                DiscountPercentage = 20
+            };
+
+            // Act
+            var result = (ObjectResult)controller.Create(updatedEntity).Result;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(400);
+        }
+        [Fact]
+        public void Updates()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<PaymentsContext>();
+            var updatedEntity = new CouponDto
+            {
+                Id = -1,
+                PromoCode= "SAVE1058",
+                DiscountPercentage=13,
+                ExpirationDate= DateTime.UtcNow,
+                AuthorId=1,
+                TourId=201
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.Update(updatedEntity).Result)?.Value as CouponDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.Id.ShouldBe(-1);
+            result.PromoCode.ShouldBe(updatedEntity.PromoCode);
+            result.ExpirationDate.ShouldNotBe(default);
+            result.DiscountPercentage.ShouldBe(updatedEntity.DiscountPercentage);
+            result.AuthorId.ShouldBe(updatedEntity.AuthorId);
+            result.TourId.ShouldBe(updatedEntity.TourId);
+            // Assert - Database
+            var storedEntity = dbContext.Coupons.FirstOrDefault(i => i.PromoCode.Equals ("SAVE1058"));
+            storedEntity.ShouldNotBeNull();
+            storedEntity.PromoCode.ShouldBe(updatedEntity.PromoCode);
+            var oldEntity = dbContext.Coupons.FirstOrDefault(i => i.PromoCode.Equals("SAVE1057"));
+            oldEntity.ShouldBeNull();
+        }
+        [Fact]
+        public void Deletes()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<PaymentsContext>();
+
+            // Act
+            var result = (OkResult)controller.Delete(-3);
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(200);
+
+            // Assert - Database
+            var storedCourse = dbContext.Coupons.FirstOrDefault(i => i.Id == -3);
+            storedCourse.ShouldBeNull();
         }
         private static CouponController CreateController(IServiceScope scope)
         {
---
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/CouponQueryTests.cs
@@ -0,0 +1,63 @@
+﻿using Explorer.API.Controllers.Author.TourAuthoring;
+using Explorer.API.Controllers.Tourist.TourShopping;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Payments.API.Dtos;
+using Explorer.Payments.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Tests.Integration
+{
+
+    [Collection("Sequential")]
+
+    public class CouponQueryTests:BasePaymentsIntegrationTest
+    {
+        public CouponQueryTests(PaymentsTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            //Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = ((ObjectResult)controller.GetAll(1,0,0).Result)?.Value as PagedResult<CouponDto>;
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Results.Count.ShouldBe(2); //samo one koji nisu istekli, u tesnim podacima ima jedan koji je istekao
+            result.TotalCount.ShouldBe(2);
+
+        }
+        [Fact]
+        public void Retrieves_one_by_tour_id()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = ((ObjectResult)controller.GetByTourId(201).Result)?.Value as CouponDto;
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Id.ShouldBe(-1);
+
+        }
+
+        private static CouponController CreateController(IServiceScope scope)
+        {
+            return new CouponController(scope.ServiceProvider.GetRequiredService<ICouponService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
