98bfad4e82baf1df7787e36e0f7683bbe319987a
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-12-06 10:59:29 +0100

    Added scripts

--- src/Explorer.API/Scripts/Stakeholders/stakeholders-data.sql
+++ src/Explorer.API/Scripts/Stakeholders/stakeholders-data.sql
@@ -1,7 +1,6 @@
 ﻿/****** STAKEHOLDERS MODULE ******/
 
 DELETE FROM stakeholders."Notification";
-DELETE FROM tours."KeyPoints";
 DELETE FROM stakeholders."Problem";
 DELETE FROM stakeholders."ClubJoinRequests";
 DELETE FROM stakeholders."ClubInvitations";
@@ -9,137 +8,50 @@ DELETE FROM stakeholders."Clubs";
 DELETE FROM stakeholders."AppReviews";
 DELETE FROM stakeholders."People";
 DELETE FROM stakeholders."Users";
-DELETE FROM tours."Objects";
 
 /* Users */
+INSERT INTO stakeholders."Users"("Id", "Username", "Password", "Role", "IsActive")
+VALUES 
+    (222, 'turista1', '12345678', 2, true),
+    (223, 'turista2', '12345678', 2, true),
+    (224, 'admin', '12345678', 0, true),
+    (225, 'autor', '12345678', 1, true),
+    (226, 'turista', '12345678', 2, true),
+    (227, 'autor2', '12345678', 1, true);
 
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-1, 'admin@gmail.com', '12345678', 0, true),
-       (-2, 'korisnik@gmail.com', '12345678', 1, true),
-       (-3, 'masa@gmail.com', '12345678', 1, true);
 
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES ( 1001, 'autor1@gmail.com', '12345678', 1, true);
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-12, 'autor2@gmail.com', '12345678', 1, true);
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-13, 'autor3@gmail.com', '12345678', 1, true);
 
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-21, 'turista1@gmail.com', '12345678', 2, true);
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-22, 'turista2@gmail.com', '12345678', 2, true);
-INSERT INTO stakeholders."Users"(
-    "Id", "Username", "Password", "Role", "IsActive")
-VALUES (-23, 'turista3@gmail.com', '12345678', 2, true);
 
 /* People */
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES (1001, 1001, 'Ana', 'Anić', 'autor1@gmail.com','https1','KaoJa', 'Samo Jako Bro', ARRAY[1, 2]::integer[], 1000);
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES 
-(-12, -12, 'Lena', 'Lenić', 'autor2@gmail.com', 'profilepic2.jpg', 'Lena loves to write about travel.', 'Explore the world.', ARRAY[1, 2]::integer[], 1000);
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES 
-(-13, -13, 'Sara', 'Sarić', 'autor3@gmail.com', 'profilepic3.jpg', 'Sara is a creative writer.', 'Imagination is key.', ARRAY[1, 2]::integer[], 1000);
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES 
-(-21, -21, 'Pera', 'Perić', 'turista1@gmail.com', 'profilepic4.jpg', 'Pera enjoys discovering new places.', 'Adventure awaits.', ARRAY[1, 2]::integer[], 1000);
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES 
-(-22, -22, 'Mika', 'Mikić', 'turista2@gmail.com', 'profilepic5.jpg', 'Mika is an avid traveler.', 'Travel more, worry less.', ARRAY[1, 2]::integer[], 1000);
-INSERT INTO stakeholders."People"(
-    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet")
-VALUES 
-(-23, -23, 'Steva', 'Stević', 'turista3@gmail.com', 'profilepic6.jpg', 'Steva enjoys nature and hiking.', 'Nature is the best teacher.', ARRAY[1, 2]::integer[], 1000);
 
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email", "ImageUrl", "Biography", "Motto", "Equipment", "Wallet", "XP", "Level")
+    VALUES (222, 222, 'Turista', 'Prvi', 'turista@gmail.com', '', NULL, NULL, NULL, 0, 0, 1),
+        (223, 223, 'Turista', 'Drugi', 'turista@gmail.com', '', NULL, NULL, NULL, 0, 0, 1),
+         (226, 226, 'Turista', 'Treci', 'turista@gmail.com', '', NULL, NULL, NULL, 0, 0, 11);
 
 /* AppReviews */
 
-INSERT INTO stakeholders."AppReviews"(
-    "Id", "UserId", "Grade", "Comment", "CreationTime")
-VALUES (-1, -1, 5, 'odlicna aplikacija', '2024-10-14 13:26:04.723');
-INSERT INTO stakeholders."AppReviews"(
-    "Id", "UserId", "Grade", "Comment", "CreationTime")
-VALUES (-2, -2, 4, 'dodati jos funkcionalnosti', '2024-10-12 15:26:04.723');
 
 /* Clubs */
 
-INSERT INTO stakeholders."Clubs"(
-    "Id", "Name", "Description", "Image", "UserId", "UserIds")
-VALUES 
-(-1, 'Mountaineering Society of Vojvodina', 'A group for everyone who wants to try new activities', 'image', -1,ARRAY[21, 23]::integer[]);
 
-INSERT INTO stakeholders."Clubs"(
-    "Id", "Name", "Description", "Image", "UserId", "UserIds")
-VALUES 
-(-2, 'Cycling Across Austria', 'A group for experienced cyclists, ready for long routes through beautiful Austria', 'image2', -11,ARRAY[21, 23]::integer[]);
 
 
 /* ClubInvitations */
 
-INSERT INTO stakeholders."ClubInvitations"(
-    "Id", "ClubId", "MemberId", "Status")
-VALUES (-1,-1, 1, 0);
-INSERT INTO stakeholders."ClubInvitations"(
-    "Id", "ClubId", "MemberId", "Status")
-VALUES (-2, -2, 2, 0);
-INSERT INTO stakeholders."ClubInvitations"(
-    "Id", "ClubId", "MemberId", "Status")
-VALUES (-3, -2, 3, 0);  
 
 
 /* ClubJoinRequests */
 
-INSERT INTO stakeholders."ClubJoinRequests"(
-	"Id", "UserId", "ClubId", "Status")
-	VALUES (1, 1, 1, 0);
-INSERT INTO stakeholders."ClubJoinRequests"(
-	"Id", "UserId", "ClubId", "Status")
-	VALUES (2, 1, 2, 1);
-INSERT INTO stakeholders."ClubJoinRequests"(
-	"Id", "UserId", "ClubId", "Status")
-	VALUES (3, 2, 1, 2);
-INSERT INTO stakeholders."ClubJoinRequests"(
-	"Id", "UserId", "ClubId", "Status")
-	VALUES (4, 2, 1, 2);
+
 
 
 /* Problems */
 
-INSERT INTO stakeholders."Problem"(
-    "Id", "UserId", "TourId", "Category", "Description", "Priority", "Time","IsActive","Deadline","Comments")
-	VALUES (-2, 2, 2, 'promena dana', 'pomeranje termina sa 3.10 na 4.10', 7, '2024-10-16 12:00:00', true,0,'[]'::jsonb);
 
-INSERT INTO stakeholders."Problem"(
-    "Id", "UserId", "TourId", "Category", "Description", "Priority", "Time", "IsActive","Deadline","Comments")
-	VALUES (-3, 2, 2, 'promena satnice', 'odlaganje pokreta za 2 sata', 2, '2024-11-16 15:00:00', true,4,'[]'::jsonb);
-
-INSERT INTO stakeholders."Problem"(
-    "Id", "UserId", "TourId", "Category", "Description", "Priority", "Time","IsActive","Deadline","Comments")
-	VALUES (-4, 1, 1, 'promena trajanja ture', 'skratiti turu za 1 dan', 9, '2024-12-16 11:00:00', true,3,'[]'::jsonb);
 
 
 
 /* Notifications */
 
-INSERT INTO stakeholders."Notification"(
-    "Id", "Description", "CreationTime", "IsRead", "UserId", "NotificationsType", "ResourceId")
-    VALUES (-1, 'E imas novi problemvau', '2024-10-12 15:26:04.723', true, -11, 0, -2);
-INSERT INTO stakeholders."Notification"(
-    "Id", "Description", "CreationTime", "IsRead", "UserId", "NotificationsType", "ResourceId")
-    VALUES (-2, 'E imas novi problem', '2024-10-12 15:26:04.723', true, -11, 0, -3);
-INSERT INTO stakeholders."Notification"(
-    "Id", "Description", "CreationTime", "IsRead", "UserId", "NotificationsType", "ResourceId")
-    VALUES (-3, 'E imas novi problem cao', '2024-10-12 15:26:04.723', false, -11, 0, -4);
--- src/Explorer.API/Scripts/Tours/tours-data.sql
+++ src/Explorer.API/Scripts/Tours/tours-data.sql
@@ -10,185 +10,140 @@ DELETE FROM tours."Equipment";
 DELETE FROM tours."Objects";
 
 /* Objects */
-INSERT INTO tours."Objects"(
-    "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId", "PublicStatus")
-VALUES (-1, 'Restoran kod Miloša', 'Tradicionalni srpski restoran poznat po specijalitetima sa roštilja.', 'restoran_milos.jpg', 1, 20.457273, 44.817613, 1001, 0);
-INSERT INTO tours."Objects"(
-    "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId", "PublicStatus")
-VALUES (-2, 'Parking Slavija', 'Javni parking sa preko 200 mesta u centru Beograda.', 'parking_slavija.jpg', 1, 20.467327, 44.799935, 1002, 0);
-INSERT INTO tours."Objects"(
-    "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId", "PublicStatus")
-VALUES (-3, 'WC Kalemegdan', 'Javni toalet na ulazu u park Kalemegdan.', 'wc_kalemegdan.jpg', 1, 20.448921, 44.823292, 1003, 0);
-
 
 /* Equipment */
 
-INSERT INTO tours."Equipment"(
-    "Id", "Name", "Description")
-VALUES (-1, 'Voda', 'Količina vode varira od temperature i trajanja ture. Preporuka je da se pije pola litre vode na jedan sat umerena fizičke aktivnosti (npr. hajk u prirodi bez značajnog uspona) po umerenoj vrućini');
-INSERT INTO tours."Equipment"(
-    "Id", "Name", "Description")
-VALUES (-2, 'Štapovi za šetanje', 'Štapovi umanjuju umor nogu, pospešuju aktivnost gornjeg dela tela i pružaju stabilnost na neravnom terenu.');
-INSERT INTO tours."Equipment"(
-    "Id", "Name", "Description")
-VALUES (-3, 'Obična baterijska lampa', 'Baterijska lampa od 200 do 400 lumena.');
 
 /* Positions */
 
 INSERT INTO tours."Positions"(
-	"Id", "Longitude", "Latitude", "TouristId")
-	VALUES (-1, 41, 21, 1);
-
-INSERT INTO tours."Positions"(
-	"Id", "Longitude", "Latitude", "TouristId")
-	VALUES (-2, 42, 22, 2);
-
-INSERT INTO tours."Positions"(
-	"Id", "Longitude", "Latitude", "TouristId")
-	VALUES (-3, 43, 23, 3);
+    "Id", "Latitude", "Longitude", "TouristId")
+VALUES 
+(111, 45.244648392133605, 19.847638305710497, 222),
+(112, 45.244648392133605, 19.847638305710497, 223);
 
 /* Tour */
 
-INSERT INTO tours."Tour" (
-    "Id", "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "LengthInKm", "PublishedTime", "ArchiveTime", "EquipmentIds"
+INSERT INTO tours."Tour" ("Id", 
+    "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "EquipmentIds", "LengthInKm", "PublishedTime", "ArchiveTime"
 )
-VALUES (
-    -1,
-    'Amazing City Tour',
-    'Explore the historical landmarks of the city with guided insights.',
-    'Easy',
-    ARRAY[6, 7, 13]::integer[], 
-    0,
-    0,  
-     1001, 
-    5.0,  
-    '2024-10-14 23:53:24.948+02', 
-    '2024-10-14 23:53:24.948+02',  
-    ARRAY[]::integer[] 
-);
+VALUES 
+    (222, 
+    'Mountain Adventure', 
+    'A thrilling hike through the scenic mountain trails.', 
+    'Moderate', 
+    ARRAY[2, 4, 8]::integer[],  
+    1, 
+    50.00, 
+    225,    --ovde treba da bude id autora ture 
+    ARRAY[]::integer[],  
+    15.5, 
+    '2024-01-15 10:00:00', 
+    NULL
+    );
+INSERT INTO tours."Tour" ("Id", 
+    "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "EquipmentIds", "LengthInKm", "PublishedTime", "ArchiveTime"
+)
+VALUES 
+    (223, 
+    'Desert Adventure', 
+    'A thrilling hike through the desert.', 
+    'Hard', 
+    ARRAY[2, 4, 8]::integer[],  
+    1, 
+    70.00, 
+    225,    --ovde treba da bude id autora ture 
+    ARRAY[]::integer[],  
+    15.5, 
+    '2024-01-15 10:00:00', 
+    NULL
+    );
 
-INSERT INTO tours."Tour" (
-    "Id", "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "LengthInKm", "PublishedTime", "ArchiveTime", "EquipmentIds"
+INSERT INTO tours."Tour" ("Id", 
+    "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "EquipmentIds", "LengthInKm", "PublishedTime", "ArchiveTime"
 )
-VALUES (
-    -2,
-    'Mountain Adventure',
-    'A challenging tour through the high mountains, suitable for experienced hikers.',
-    'Hard',
-    ARRAY[2, 3, 5]::integer[], 
-    1,
-    0,  
-     1001, 
-    10.0,  
-    '2024-10-14 23:53:24.948+02',  
-    '2024-10-14 23:53:24.948+02',  
-    ARRAY[-3, -1]::integer[]  
-);
+VALUES 
+     (224, 
+    'Forest Adventure', 
+    'A thrilling hike through the forest.', 
+    'Hard', 
+    ARRAY[2, 4, 8]::integer[],  
+    1, 
+    40.00, 
+    225,    --ovde treba da bude id autora ture 
+    ARRAY[]::integer[],  
+    15.5, 
+    '2024-01-15 10:00:00', 
+    NULL
+    );
 
-INSERT INTO tours."Tour" (
-    "Id", "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "LengthInKm", "PublishedTime", "ArchiveTime", "EquipmentIds"
+INSERT INTO tours."Tour" ("Id", 
+    "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "EquipmentIds", "LengthInKm", "PublishedTime", "ArchiveTime"
 )
-VALUES (
-    -4,  
-    'Mountain Challenge',
-    'An adventurous hike through the mountain trails, perfect for thrill-seekers.',
-    'Hard',
-    ARRAY[2, 3, 5]::integer[], 
-    2,
-    100.0,  
-     1001, 
-    15.0,  
-    '2024-10-14 23:53:24.948+02',  
-    '2024-10-14 23:53:24.948+02',  
-    ARRAY[-3, -1]::integer[]   
-);
-INSERT INTO tours."Tour" (
-    "Id", "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "LengthInKm", "PublishedTime", "ArchiveTime", "EquipmentIds"
+VALUES 
+     (225, 
+    'Forest Adventure 2', 
+    'A thrilling hike through the forest.', 
+    'Hard', 
+    ARRAY[2, 4, 8]::integer[],  
+    1, 
+    40.00, 
+    227,    --ovde treba da bude id autora ture 
+    ARRAY[]::integer[],  
+    15.5, 
+    '2024-01-15 10:00:00', 
+    NULL
+    );
+
+INSERT INTO tours."Tour" ("Id", 
+    "Name", "Description", "Difficulty", "Tags", "Status", "Price", "UserId", "EquipmentIds", "LengthInKm", "PublishedTime", "ArchiveTime"
 )
-VALUES (
-    -5,  
-    'Safari Challenge',
-    'PERFECT SAFARI TOUR',
-    'Hard',
-    ARRAY[2, 3, 5]::integer[], 
-    2,
-    100.0,  
-    1001, 
-    15.0,  
-    '2024-10-14 23:53:24.948+02',  
-    '2024-10-14 23:53:24.948+02',  
-    ARRAY[-3, -1]::integer[]
-);
+VALUES 
+    (226, 
+    'Desert Adventure 2', 
+    'A thrilling hike through the desert.', 
+    'Hard', 
+    ARRAY[2, 4, 8]::integer[],  
+    1, 
+    70.00, 
+    227,    --ovde treba da bude id autora ture 
+    ARRAY[]::integer[],  
+    15.5, 
+    '2024-01-15 10:00:00', 
+    NULL
+    );
 
 
 /* TourPreferences */
 
-INSERT INTO tours."TourPreferences"(
-    "Id", "TouristId", "WeightPreference", "WalkingRating", "BikeRating", "CarRating", "BoatRating", "Tags")
-VALUES (1, 1, 2, 2, 2, 2, 2, '{{"creativity"}}');
-INSERT INTO tours."TourPreferences"(
-    "Id", "TouristId", "WeightPreference", "WalkingRating", "BikeRating", "CarRating", "BoatRating", "Tags")
-VALUES (2, 2, 3, 3, 3, 2, 2, '{{"scary"}}');
+
 
 /* KeyPoints */
 
-INSERT INTO tours."KeyPoints"(
-	"Id", "Name", "Longitude", "Latitude", "Description", "Image", "UserId", "TourId", "PublicStatus")
-	VALUES (-1, 'Colosseum, Rome', 12.4922, 41.8902, 'It is located just a few hundred meters away from the city center, at the northernmost point of Rome historic center.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPd_FbzQUocKtjn8O2DFiQIrVQcZQgP5BU1MWzaDSMrw&s', 2, -1, 0);
-
-INSERT INTO tours."KeyPoints"(
-	"Id", "Name", "Longitude", "Latitude", "Description", "Image", "UserId", "TourId", "PublicStatus")
-	VALUES (-2, 'Chiesa di Sant Ignazio di Loyola, Rome', 12.4797, 41.8992,  'The Church of St. Ignatius of Loyola at Campus Martius is a Latin Catholic titular church, of deaconry rank, dedicated to Ignatius of Loyola, the founder of the Society of Jesus, located in Rome, Italy.', 'https://d1c233nw6edifh.cloudfront.net/wp-content/uploads/sites/60/2018/06/volta-chiesa-sant-ignazio-roma.jpg', 2, -2,0);
-
-INSERT INTO tours."KeyPoints"(
-	"Id", "Name", "Longitude", "Latitude", "Description", "Image", "UserId", "TourId", "PublicStatus")
-	VALUES (-3, 'Sistine Chapel, Vatican City', 12.4545, 41.9029,  'Sistine Chapel is a large historic chapel and one of the papal facilities located in Vatican City and famous with its amazing frescoes made by Michelangelo and other artists.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Sistina-interno.jpg/640px-Sistina-interno.jpg', 2, -1,0);
+INSERT INTO tours."KeyPoints" ("Id", "Name", "Longitude", "Latitude", "Description", "Image", "PublicStatus", "UserId", "TourId") VALUES
+(111, 'Random KP', 19.84494832522442, 45.255041845831364, 'Random Keypoint', 'image1.jpg', 0, 60, 222),
+(112, 'Random KP', 19.84494832522442, 45.3, 'Random Keypoint', 'image1.jpg', 0, 60, 222),
+(113, 'Random KP', 19.84494832522442, 45.255041845831364, 'Random Keypoint', 'image1.jpg', 0, 60, 223),
+(114, 'Random KP', 19.84494832522442, 45.3, 'Random Keypoint', 'image1.jpg', 0, 60, 223),
+(115, 'Random KP', 19.84494832522442, 45.255041845831364, 'Random Keypoint', 'image1.jpg', 0, 60, 224),
+(116, 'Random KP', 19.84494832522442, 45.3, 'Random Keypoint', 'image1.jpg', 0, 60, 224),
+(117, 'Random KP', 19.84494832522442, 45.255041845831364, 'Random Keypoint', 'image1.jpg', 0, 60, 225),
+(118, 'Random KP', 19.84494832522442, 45.3, 'Random Keypoint', 'image1.jpg', 0, 60, 225),
+(119, 'Random KP', 19.84494832522442, 45.255041845831364, 'Random Keypoint', 'image1.jpg', 0, 60, 226),
+(120, 'Random KP', 19.84494832522442, 45.3, 'Random Keypoint', 'image1.jpg', 0, 60, 226);
 
 
 /* PublicKeypointRequests */
 
-INSERT INTO tours."KeyPoints"(
-	"Id", "Name", "Longitude", "Latitude", "Description", "Image", "UserId", "TourId", "PublicStatus")
-	VALUES (-4, 'Sistine Chapel, Vatican City', 12.4545, 41.9029,  'Sistine Chapel is a large historic chapel and one of the papal facilities located in Vatican City and famous with its amazing frescoes made by Michelangelo and other artists.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Sistina-interno.jpg/640px-Sistina-interno.jpg', 4, -1,0);
-INSERT INTO tours."Objects"(
-    "Id", "Name", "Description", "Image", "Category", "Longitude", "Latitude", "UserId", "PublicStatus")
-	VALUES (-5, 'WC Kalemegdan', 'Javni toalet na ulazu u park Kalemegdan.', 'wc_kalemegdan.jpg', 1, 20.448921, 44.823292, 1003, 0);
+
 
 /* TourReviews */
 
-INSERT INTO tours."TourReview"(
-	"Id", "IdTour", "IdTourist", "Rating", "Comment", "DateTour", "DateComment", "Images")
-	VALUES (-1, 1, 1, 5, 'Mnogo dobra tura.', '2024-10-14 23:53:24.948+02', '2024-10-14 23:53:24.948+02', ARRAY[]::integer[]);
-INSERT INTO tours."TourReview"(
-	"Id", "IdTour", "IdTourist", "Rating", "Comment", "DateTour", "DateComment", "Images")
-	VALUES (-2, 1, 1, 4, 'Dobra tura.', '2024-10-14 23:53:24.948+02', '2024-10-14 23:53:24.948+02', ARRAY[]::integer[]);
-INSERT INTO tours."TourReview"(
-	"Id", "IdTour", "IdTourist", "Rating", "Comment", "DateTour", "DateComment", "Images")
-	VALUES (-3, 1, 1, 1, 'Mnogo losa tura.', '2024-10-14 23:53:24.948+02', '2024-10-14 23:53:24.948+02', ARRAY[]::integer[]);
+
 
 /* TourExecutions */
+INSERT INTO tours."TourExecution" ("Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys") VALUES
+(111, 222, 222, 111, '2024-11-03 10:00:00', 0, '[]'),
+(112, 222, 223, 112, '2024-11-03 10:00:00', 0, '[]');
 
-INSERT INTO tours."TourExecution"(
-    "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
-VALUES 
-    (-1, -1, -22, -1, '2024-10-14 23:53:24.948+02', 0, 
-        '[]');
-
-INSERT INTO tours."TourExecution"(
-    "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
-VALUES 
-    (-2, -1, -21, -2, '2024-10-14 23:53:24.948+02', 1, 
-        '[]');
-
-INSERT INTO tours."TourExecution"(
-    "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
-VALUES 
-    (-3, -2, -23, -1, '2024-10-14 23:53:24.948+02', 0, 
-        '[]');
-
-INSERT INTO tours."TourExecution"(
-    "Id", "TourId", "TouristId", "LocationId", "LastActivity", "Status", "CompletedKeys")
-VALUES 
-    (-5, -2, -23, -1, '2024-10-14 23:53:24.948+02', 0, 
-        '[]');
 
