11ac5ce22b9f5aa19119e9a1ea61d98a8a6040d0
Damjan Ilic <damjanilic02@gmail.com>
2025-07-27 10:18:22 +0200

    feat: secret biznis

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ClubDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ClubDto.cs
@@ -14,7 +14,7 @@ namespace Explorer.Stakeholders.API.Dtos
         public string? Image { get; set; }
         public long UserId { get; set; }
         public List<long> UserIds {get; set;}
-        public string ImageBase64 {  get; set; }
+        public string? ImageBase64 {  get; set; }
 
         public List<ClubTags> Tags { get; set; }
 
